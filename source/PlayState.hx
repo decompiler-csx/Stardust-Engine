48b9e2b9b257b20e435bb179f5e6da21043cd20a
Tyler <93739827+TylerTehIdiot@users.noreply.github.com>
2022-06-15 08:47:14 -0400

    custom botplay text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1148,7 +1148,7 @@ class PlayState extends MusicBeatState
 		add(scoreTxt);
 
 		if (!inEditor) {
-			botplayTxt = new FlxText(400, timeBarBG.y + 55, FlxG.width - 800, "BOTPLAY", 32);
+			botplayTxt = new FlxText(400, timeBarBG.y + 55, FlxG.width - 800, "Noob Mode", 32);
 			botplayTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 			botplayTxt.scrollFactor.set();
 			botplayTxt.borderSize = 1.25;
