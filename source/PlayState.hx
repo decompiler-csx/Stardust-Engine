5b2de7620d58eaea9b47381dd72e8506dea341bc
Tyler <93739827+TylerTehIdiot@users.noreply.github.com>
2022-07-29 21:06:35 -0400

    Test

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -54,16 +54,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Good', 0.2], //From 0% to 19%
+		['Great', 0.4], //From 20% to 39%
+		['Awesome', 0.5], //From 40% to 49%
+		['Outstandjng', 0.6], //From 50% to 59%
+		['AMAZING!', 0.69], //From 60% to 68%
+		['funny sex position, 0.7], //69%
+		['Meh my brother could do better', 0.8], //From 70% to 79%
+		['Deez', 0.9], //From 80% to 89%
+		['ok', 1], //From 90% to 99%
+		['HOLY SHIT YOU HIT A CLIP', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	public var modchartTweens:Map<String, FlxTween> = new Map();
