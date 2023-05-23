b893e06e5783290abb326d6b556a56117b6f5bf2
ティデヴ <108774265+TyDevX@users.noreply.github.com>
2023-05-23 12:20:15 -0400

    I got lied to

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -12,7 +12,7 @@ import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.tweens.FlxTween;
-import flixel.sound.FlxSound;
+import flixel.system.FlxSound;
 import flash.media.Sound;
 #if MODS_ALLOWED
 import sys.io.File;
