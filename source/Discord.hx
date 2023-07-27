02c3f9f578cff6e323804facf06eab74d41bec3a
SnesFX <108774265+TyDevX@users.noreply.github.com>
2023-07-27 12:32:44 -0400

    rpc fix and remove monika from the files

--- source/Discord.hx
+++ source/Discord.hx
@@ -18,7 +18,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "1041373078254530592",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
